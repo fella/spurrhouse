25c91d39da906ae4c3f2e43d8a6d968026c046c1
fella <quicksandee@gmail.com>
2024-03-16 09:23:56 -0700

    save dev wp-config file

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'DB_USER', 'spurrhousedb' );
 define( 'DB_PASSWORD', 'Sup3rf0xy!' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
