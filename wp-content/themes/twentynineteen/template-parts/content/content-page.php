5d997a3806892055d87303fe07d9e4a47b7c3028
fella <quicksandee@gmail.com>
2024-03-21 16:39:54 -0700

    new feature

--- wp-content/themes/twentynineteen/template-parts/content/content-page.php
+++ wp-content/themes/twentynineteen/template-parts/content/content-page.php
@@ -20,7 +20,7 @@
 
 	<div class="entry-content">
 		<?php echo '<h3New Feature</h3>';?>
-			<p>This feature is called 12:55</p>
+			<p style="font-size:20px;font-weight:700">Y this is one heck-a-vuh feature, HODAD!</p>
 		<?php
 		the_content();
 
